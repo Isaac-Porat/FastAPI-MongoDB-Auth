0c9e2788264d7918915e57e273f6201d10dcb463
ZakPorat0 <zakporat2023@gmail.com>
2024-06-13 18:51:52 +0100

    Added auth routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,12 @@
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from pydantic import BaseModel
 from pymongo import MongoClient
 from contextlib import asynccontextmanager
 from config import MONGODB_COLLECTION_NAME, MONGODB_DATABASE_NAME, MONGODB_DATABASE_URL
+from models import User
+from auth import login_for_access_token, register_for_access_token
 
 app = FastAPI()
 
@@ -37,6 +38,14 @@ if environment == "dev":
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/register")
+async def register(user: User):
+    register_for_access_token(user)
+
+@app.post("/login")
+async def login(user: User):
+    login_for_access_token(user)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
