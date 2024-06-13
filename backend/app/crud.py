b3d676e79e7f868df6bfd8c449b2c5a301e67f09
ZakPorat0 <zakporat2023@gmail.com>
2024-06-13 18:51:16 +0100

    Fixed create_user

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -11,18 +11,11 @@ collection = app.collection
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_user(user: User):
-  try:
-    user.hashed_password = get_password_hash(user.password)
+  user.hashed_password = get_password_hash(user.password)
 
-    result = collection.insert_one(user.model_dump())
+  result = collection.insert_one(user.model_dump())
 
-    user_id = result.inserted_id
-
-    return {
-      "message": "User created successfully", "id": str(user_id)
-    }
-  except ValidationError as e:
-    raise HTTPException(status_code=400, detail=f"Error inserting user into collection: {e}")
+  return result.inserted_id
 
 def authenticate_user(username: str, password: str):
     user = collection.find_one({"username": username})
