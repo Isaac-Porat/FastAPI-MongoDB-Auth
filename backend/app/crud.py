3ddf5a6a7338050ced3633808e69e1bf21d7347b
Isaac Porat <117609055+Isaac-Porat@users.noreply.github.com>
2024-06-12 18:40:46 -0700

    Create crud.py

---
+++ backend/app/crud.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+from fastapi import HTTPException
+from pydantic import BaseModel, ValidationError
+from passlib.context import CryptContext
+from main import app.collection
+from models import User
+from auth import get_password_hash
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_user(user: User):
+  try:
+    user.hashed_password = get_password_hash(user.password)
+
+    result = app.collection.insert_one(user.model_dump())
+
+    user_id = result.inserted_id
+
+    return {
+      "message": "User created successfully", "id": str(user_id)
+    }
+  except ValidationError as e:
+    raise HTTPException(status_code=400, detail=f"Error inserting user into collection: {e}")
+
+def authenticate_user(username: str, password: str):
+    user = app.collection.find_one({"username": username})
+    if user and pwd_context.verify(password, user['hashed_password']):
+        return user
+    return None
+
