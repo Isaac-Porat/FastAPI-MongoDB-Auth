ee8c64f3cdd091c3c32b258d0d88c5dd8a016eb4
Isaac Porat <117609055+Isaac-Porat@users.noreply.github.com>
2024-06-12 18:40:27 -0700

    Added cors and lifetime context manager

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,38 @@
+import logging
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from pydantic import BaseModel
+from pymongo import MongoClient
+from contextlib import asynccontextmanager
+from config import MONGODB_COLLECTION_NAME, MONGODB_DATABASE_NAME, MONGODB_DATABASE_URL
 
 app = FastAPI()
+
+@asynccontextmanager
+async def lifetime(app: FastAPI):
+    app.client = MongoClient(MONGODB_DATABASE_URL)
+
+    app.db = app.client[MONGODB_DATABASE_NAME]
+
+    app.collection = app.db[MONGODB_COLLECTION_NAME]
+
+    yield
+
+    app.client.close()
+
+environment = "dev"
+
+if environment == "dev":
+    logger = logging.getLogger("uvicorn")
+    logger.warning("Running in development mode - allowing CORS for all origins")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 @app.get("/")
 async def root():
