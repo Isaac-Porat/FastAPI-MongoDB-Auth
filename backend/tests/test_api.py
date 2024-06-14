9245784da7d77ce6c7161314e01c7820517a1775
ZakPorat0 <zakporat2023@gmail.com>
2024-06-14 20:54:55 +0100

    Updated tests

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -2,7 +2,7 @@ import requests
 
 def register_test_api():
     try:
-        register_attempt = requests.post("http://localhost:8000/register", data={"username": "testuser5", "password": "testpassword"})
+        register_attempt = requests.post("http://localhost:8000/register", data={"username": "testuser30", "password": "testpassword"})
         register_attempt.raise_for_status()
         print(register_attempt.json())
     except requests.exceptions.HTTPError as err:
@@ -12,7 +12,7 @@ def register_test_api():
 
 def login_test_api():
     try:
-        login_attempt = requests.post("http://localhost:8000/login", data={"username": "testuser2", "password": "testpassword"})
+        login_attempt = requests.post("http://localhost:8000/login", data={"username": "testuser30", "password": "testpassword"})
         login_attempt.raise_for_status()
         print(login_attempt.json())
     except requests.exceptions.HTTPError as err:
@@ -22,4 +22,4 @@ def login_test_api():
 
 if __name__ == "__main__":
     register_test_api()
-    # login_test_api()
+    login_test_api()
