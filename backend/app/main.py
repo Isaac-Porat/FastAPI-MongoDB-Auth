19b14e852d0ae41e00c0857e7507e86bb05311f0
ZakPorat0 <zakporat2023@gmail.com>
2024-06-18 00:12:25 +0100

    Refractored routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,44 +1,25 @@
 import os
 import logging
 from dotenv import load_dotenv
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordRequestForm
 import uvicorn
-from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
 from models import Token
+from motor.motor_asyncio import AsyncIOMotorClient
 from auth import register_user, login_user, get_current_user
 from admin import create_admin_user, get_current_admin_user
+from config import MONGODB_DATABASE_URL, MONGODB_DATABASE_NAME, MONGODB_COLLECTION_NAME
+from database import MongoMiddleware
 
 load_dotenv()
 
 logger = logging.getLogger("uvicorn")
 
-# Initialize MongoDB client and collection
-MONGODB_DATABASE_URL = os.getenv("MONGODB_DATABASE_URL")
-MONGODB_DATABASE_NAME = os.getenv("MONGODB_DATABASE_NAME")
-MONGODB_COLLECTION_NAME = os.getenv("MONGODB_COLLECTION_NAME")
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    try:
-        app.client = AsyncIOMotorClient(MONGODB_DATABASE_URL)
-        app.db = app.client[MONGODB_DATABASE_NAME]
-        app.collection = app.db.get_collection(MONGODB_COLLECTION_NAME)
-
-        logger.warning(f"Connected to MongoDB collection: {app.collection}")
-    except Exception as e:
-        logger.error(f"Failed to connect to MongoDB: {e}")
-        raise
-
-    create_admin_user(app.collection)
-
-    yield
-
-    app.client.close()
-
-app = FastAPI(lifespan=lifespan)
+app.add_middleware(MongoMiddleware, db_url=MONGODB_DATABASE_URL, db_name=MONGODB_DATABASE_NAME, collection_name=MONGODB_COLLECTION_NAME)
 
 # Initialize development environment
 environment = "dev"
@@ -56,18 +37,34 @@ if environment == "dev":
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/test-middleware")
+async def test_middleware(request: Request):
+    if hasattr(request.state, 'collection'):
+        return {"message": "Middleware is working"}
+    else:
+        return {"message": "Middleware is not working"}
+
+@app.get("/create-admin")
+async def create_admin(request: Request):
+    await create_admin_user(request)
+
 @app.post("/register", response_model=Token)
 async def register(user: OAuth2PasswordRequestForm = Depends()) -> Token:
-    access_token = await register_user(app.collection, user)
+    access_token = await register_user(user)
     return access_token
 
 @app.post("/login", response_model=Token)
-async def login(user: OAuth2PasswordRequestForm = Depends()) -> Token:
-   access_token = await login_user(app.collection, user)
+async def login(request: Request, user: OAuth2PasswordRequestForm = Depends()) -> Token:
+   access_token = await login_user(request, user)
+   return access_token
+
+@app.post("/token", response_model=Token)
+async def login(request: Request, user: OAuth2PasswordRequestForm = Depends()) -> Token:
+   access_token = await login_user(request, user)
    return access_token
 
 @app.get("/users/me")
-async def get_current_active_user(token: str = Depends(get_current_user)):
+async def get_current_active_user(request: Request, token: str = Depends(get_current_user)):
     return token
 
 @app.get("/admin/me")
